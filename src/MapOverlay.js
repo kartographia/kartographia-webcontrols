bc431ed4d6a6f9120403f1ba27e9c4c20283a953
pborissow <peter.borissow@yahoo.com>
2020-05-06 06:48:50 -0400

    Updated MapOverlay to call Utils.js to get coordinates along a track

--- src/MapOverlay.js
+++ src/MapOverlay.js
@@ -70,6 +70,9 @@ com.kartographia.MapOverlay = function(map) {
     };
 
 
+  //**************************************************************************
+  //** remove
+  //**************************************************************************
     this.remove = function(){
         var canvas = mapTile.getCanvas();
         map.el.removeChild(canvas);
@@ -93,6 +96,7 @@ com.kartographia.MapOverlay = function(map) {
     this.addPoint = function(lat, lon, color, size, label){
         mapTile.addPoint(lat, lon, color, size, label);
     };
+
 
   //**************************************************************************
   //** addLine
@@ -258,118 +262,7 @@ com.kartographia.MapOverlay = function(map) {
   //** getCurrCoord
   //**************************************************************************
     var getCurrCoord = function(track, currTime){
-        var coordinateList = track.coords;
-
-        var currCoord = null;
-        var nextCoord = null;
-        var prevCoord = null;
-
-        for (var j=0; j<coordinateList.length; j++){
-            var coord = coordinateList[j];
-            var t = coord[2];
-            if (t>currTime){
-                nextCoord = coord;
-                if (j>0){
-                    prevCoord = coordinateList[j-1];
-                    var t2 = prevCoord[2];
-                    if (t2==t){
-                        currCoord = prevCoord;
-                        if (j>1){
-                            prevCoord = coordinateList[j-2];
-                        }
-                        else{
-                            prevCoord = null;
-                        }
-                    }
-                }
-                break;
-            }
-        }
-
-        if (prevCoord!=null){
-            var c1 = getLatLon(prevCoord[0], prevCoord[1]); //[x, y]
-            var t1 = prevCoord[2];
-            var bearing;
-            var speed;
-
-            if (prevCoord.length<4){
-
-                var c2;
-                var t2;
-                if (currCoord!=null){
-                    c2 = getLatLon(currCoord[0], currCoord[1]);
-                    t2 = currCoord[2];
-                }
-                else{
-                    c2 = getLatLon(nextCoord[0], nextCoord[1]);
-                    t2 = nextCoord[2];
-                }
-
-                var distance = getDistance(c1,c2); //meters
-                var time = (t2-t1)/1000; //seconds
-                speed = distance/time; //meters/second
-                bearing = getBearing(c1, c2);
-                prevCoord.push(bearing);
-                prevCoord.push(speed);
-            }
-            else{
-                bearing = prevCoord[3];
-                speed = prevCoord[4];
-            }
-
-            var elapsedTime = (currTime-t1)/1000; //in seconds
-            var distanceFromPrevPoint = speed*elapsedTime;
-            try{
-            currCoord = getCoordinate(c1, distanceFromPrevPoint, bearing);
-            currCoord.push(currTime);
-            currCoord.push(bearing);
-            currCoord.push(speed);
-            }
-            catch(e){}
-        }
-
-        return currCoord;
-    };
-
-
-  //**************************************************************************
-  //** getLatLon
-  //**************************************************************************
-  /** Create a Vicenty LatLon coordinate using an x,y point
-   */
-    var getLatLon = function(x, y){
-        return window.getLatLon(y, x);
-    };
-
-
-  //**************************************************************************
-  //** getDistance
-  //**************************************************************************
-  /** Returns distance between 2 points in meters. Uses Vicenty ellipsoidal
-   *  calculations.
-   */
-    var getDistance = function(c1, c2){
-        return c1.distanceTo(c2);
-    };
-
-
-  //**************************************************************************
-  //** getBearing
-  //**************************************************************************
-    var getBearing = function(c1, c2){
-        var bearing = c1.initialBearingTo(c2);
-        return isNaN(bearing) ? 0 : bearing;
-    };
-
-
-  //**************************************************************************
-  //** getCoordinate
-  //**************************************************************************
-  /** Returns x,y coordinate
-   */
-    var getCoordinate = function(p1, distance, bearing){
-        var p2 = p1.destinationPoint(distance, bearing);
-        return [p2.longitude, p2.latitude];
+        return com.kartographia.utils.getCoord(track.coords, currTime);
     };
 
 
